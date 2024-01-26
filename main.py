cd950c46725e54bd96e8c0d820cabf772dec05ff
MURAKI Taichi <muraki.taichi@gmail.com>
2024-01-26 14:39:12 +0900

    fix

--- main.py
+++ main.py
@@ -10,4 +10,23 @@ from segmentation_models_pytorch.datasets import SimpleOxfordPetDataset
 
 # download data
 root = "."
-SimpleOxfordPetDataset.download(root)
+# SimpleOxfordPetDataset.download(root)
+
+# init train, val, test sets
+train_dataset = SimpleOxfordPetDataset(root, "train")
+valid_dataset = SimpleOxfordPetDataset(root, "valid")
+test_dataset = SimpleOxfordPetDataset(root, "test")
+
+# It is a good practice to check datasets don`t intersects with each other
+assert set(test_dataset.filenames).isdisjoint(set(train_dataset.filenames))
+assert set(test_dataset.filenames).isdisjoint(set(valid_dataset.filenames))
+assert set(train_dataset.filenames).isdisjoint(set(valid_dataset.filenames))
+
+print(f"Train size: {len(train_dataset)}")
+print(f"Valid size: {len(valid_dataset)}")
+print(f"Test size: {len(test_dataset)}")
+
+n_cpu = os.cpu_count()
+train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=n_cpu)
+valid_dataloader = DataLoader(valid_dataset, batch_size=16, shuffle=False, num_workers=n_cpu)
+test_dataloader = DataLoader(test_dataset, batch_size=16, shuffle=False, num_workers=n_cpu)
