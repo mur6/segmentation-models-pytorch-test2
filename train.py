c72eef81ae65ca78a2f552f1f132a0798b1b4c94
MURAKI Taichi <muraki.taichi@gmail.com>
2024-02-01 16:37:46 +0900

    fix

--- train.py
+++ train.py
@@ -52,88 +52,94 @@ def show_samples(train_dataset, valid_dataset, test_dataset):
 
 
 def train_main():
-    wandb.init(project="segmentation_models_pytorch2")
-    config = wandb.config
-    wandb_logger = WandbLogger(log_model="all")
-    epochs = 10
-    batch_size = config.batch_size
-    # -----------------------------------
-    # wandb_logger.experiment.config.update(
-    #     {
-    #         "learning_rate": learning_rate,
-    #         "architecture": architecture,
-    #         "encoder_name": encoder_name,
-    #         "epochs": epochs,
-    #     }
-    # )
-    # -----------------------------------
-    #     # set the wandb project where this run will be logged
-    #     project="segmentation_models_pytorch",
-    #     # track hyperparameters and run metadata
-    #     config={
-    #         "learning_rate": learning_rate,
-    #         "architecture": architecture,
-    #         "encoder_name": encoder_name,
-    #         "epochs": epochs,
-    #     },
-    # )
+    config_defaults = {"learning_rate": 0.0001, "batch_size": 16}
+    with wandb.init(
+        config=config_defaults, project="segmentation_models_pytorch2"
+    ) as run:
+        config = wandb.config
+        wandb_logger = WandbLogger(log_model="all")
+        epochs = 10
+        batch_size = config.batch_size
+        # -----------------------------------
+        # wandb_logger.experiment.config.update(
+        #     {
+        #         "learning_rate": learning_rate,
+        #         "architecture": architecture,
+        #         "encoder_name": encoder_name,
+        #         "epochs": epochs,
+        #     }
+        # )
+        # -----------------------------------
+        #     # set the wandb project where this run will be logged
+        #     project="segmentation_models_pytorch",
+        #     # track hyperparameters and run metadata
+        #     config={
+        #         "learning_rate": learning_rate,
+        #         "architecture": architecture,
+        #         "encoder_name": encoder_name,
+        #         "epochs": epochs,
+        #     },
+        # )
 
-    # base_path = Path("/Users/taichi.muraki/workspace/Python/ring-finger-semseg/data/")
-    base_path = Path("../ring-finger-semseg/")
-    train_dataset = ImageSegmentationDataset(
-        root_dir=base_path / "outputs/training/",
-        transforms=None,
-    )
-    valid_dataset = ImageSegmentationDataset(
-        root_dir=base_path / "outputs/validation/",
-        transforms=None,
-        train=False,
-    )
-    # # init train, val, test sets
-    # train_dataset = SimpleOxfordPetDataset(root, "train")
-    # valid_dataset = SimpleOxfordPetDataset(root, "valid")
-    # test_dataset = SimpleOxfordPetDataset(root, "test")
+        # base_path = Path("/Users/taichi.muraki/workspace/Python/ring-finger-semseg/data/")
+        base_path = Path("../ring-finger-semseg/")
+        train_dataset = ImageSegmentationDataset(
+            root_dir=base_path / "outputs/training/",
+            transforms=None,
+        )
+        valid_dataset = ImageSegmentationDataset(
+            root_dir=base_path / "outputs/validation/",
+            transforms=None,
+            train=False,
+        )
+        # # init train, val, test sets
+        # train_dataset = SimpleOxfordPetDataset(root, "train")
+        # valid_dataset = SimpleOxfordPetDataset(root, "valid")
+        # test_dataset = SimpleOxfordPetDataset(root, "test")
 
-    # # It is a good practice to check datasets don`t intersects with each other
-    # assert set(test_dataset.filenames).isdisjoint(set(train_dataset.filenames))
-    # assert set(test_dataset.filenames).isdisjoint(set(valid_dataset.filenames))
-    # assert set(train_dataset.filenames).isdisjoint(set(valid_dataset.filenames))
+        # # It is a good practice to check datasets don`t intersects with each other
+        # assert set(test_dataset.filenames).isdisjoint(set(train_dataset.filenames))
+        # assert set(test_dataset.filenames).isdisjoint(set(valid_dataset.filenames))
+        # assert set(train_dataset.filenames).isdisjoint(set(valid_dataset.filenames))
 
-    print(f"Train size: {len(train_dataset)}")
-    print(f"Valid size: {len(valid_dataset)}")
+        print(f"Train size: {len(train_dataset)}")
+        print(f"Valid size: {len(valid_dataset)}")
 
-    n_cpu = os.cpu_count()
-    print(f"Number of CPUs: {n_cpu}")
+        n_cpu = os.cpu_count()
+        print(f"Number of CPUs: {n_cpu}")
 
-    train_dataloader = DataLoader(
-        train_dataset, batch_size=batch_size, shuffle=True, num_workers=n_cpu
-    )
-    valid_dataloader = DataLoader(
-        valid_dataset, batch_size=batch_size, shuffle=False, num_workers=n_cpu
-    )
-    # test_dataloader = DataLoader(
-    #     test_dataset, batch_size=batch_size, shuffle=False, num_workers=n_cpu
-    # )
+        train_dataloader = DataLoader(
+            train_dataset, batch_size=batch_size, shuffle=True, num_workers=n_cpu
+        )
+        valid_dataloader = DataLoader(
+            valid_dataset, batch_size=batch_size, shuffle=False, num_workers=n_cpu
+        )
+        # test_dataloader = DataLoader(
+        #     test_dataset, batch_size=batch_size, shuffle=False, num_workers=n_cpu
+        # )
 
-    # show_samples(train_dataset, valid_dataset, test_dataset)
-    model = PetModel(
-        config,
-        in_channels=3,
-        out_classes=1,
-    )
-    # Log gradients, parameters and model topology
-    wandb_logger.watch(model, log="all")
-    checkpoint_callback = ModelCheckpoint(monitor="valid_accuracy", mode="max")
-    # Training
-    trainer = pl.Trainer(
-        gpus=1, max_epochs=epochs, logger=wandb_logger, callbacks=[checkpoint_callback]
-    )
+        # show_samples(train_dataset, valid_dataset, test_dataset)
+        model = PetModel(
+            config,
+            in_channels=3,
+            out_classes=1,
+        )
+        # Log gradients, parameters and model topology
+        wandb_logger.watch(model, log="all")
+        checkpoint_callback = ModelCheckpoint(monitor="valid_accuracy", mode="max")
+        # Training
+        trainer = pl.Trainer(
+            gpus=1,
+            max_epochs=epochs,
+            logger=wandb_logger,
+            callbacks=[checkpoint_callback],
+        )
 
-    trainer.fit(
-        model,
-        train_dataloaders=train_dataloader,
-        val_dataloaders=valid_dataloader,
-    )
+        trainer.fit(
+            model,
+            train_dataloaders=train_dataloader,
+            val_dataloaders=valid_dataloader,
+        )
 
 
 def do_wandb_sweep():
@@ -165,6 +171,10 @@ def do_wandb_sweep():
     wandb.agent(sweep_id, function=train_main, count=30)
 
 
-# call main
+def main():
+    # do_wandb_sweep()
+    train_main()
+
+
 if "__main__" == __name__:
-    do_wandb_sweep()
+    main()
